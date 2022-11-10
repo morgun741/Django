ff83268a34927b6f6266c4b99377a5a635a6e265
Oleg <morgunoleg2002@gmail.com>
2022-11-10 13:40:56 +0300

    after apdate bd

--- server/products/models.py
+++ server/products/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 
-class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE(K))
-    name = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
-    cost = models.DecimalField(max_digits=12, decimal_places=2, default=0)
-    def __str__(self):
-        return self.name
-
 class Category(models.Model):
     name = models.CharField(verbose_name='имя', max_length=64, unique=True)
     description = models.TextField(verbose_name='описание', blank=True)
     def __str__(self):
         return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True)
+    name = models.CharField(verbose_name='имя продукта', max_length=128)
+    image = models.ImageField(upload_to="images", blank=True)
+    description = models.TextField(verbose_name="описание продукта", blank=True)
+    price = models.DecimalField(verbose_name="цена продукта", max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(verbose_name="количество на складе", default=0)
+
+    def __str__(self):
+        return self.name
+
