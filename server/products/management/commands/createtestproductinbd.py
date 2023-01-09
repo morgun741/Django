9f885c81896149b47da491710d5b7d0b990d9af3
morgun741 <morgunoleg2002@gmail.com>
2023-01-09 17:02:49 +0300

    h5tes

---
+++ server/products/management/commands/createtestproductinbd.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand, CommandError
+from products.models import Product
+
+#создаидим класс для перечисления имеющихся аргументов в командной строке
+class Command(BaseCommand):
+    # создадим django-метод handel в котором и будет содержаться вся логика
+    #создаваемой команды
+    #в кочестве аргументов этот  метод будет принимать контекст
+    #а также набор именованых и перечисляемых атребутов(**kwargs)
+    def handle(self, *args, **kwargs):
+        for idx in range(1, 10):
+            product_name = '[test]-product-%s' % idx
+            Product.objects.create(name=product_name)
+            self.stdout.write(self.style.SUCCESS(product_name))
--- server/products/views.py
+++ server/products/views.py
@@ -2,17 +2,22 @@ from django.shortcuts import render
 import json
 from .models import Product
 from .models import Category
+
+
 def products_list_views(request):
-    #with open('products/fixtures/data/data.json') as file :
-        #data = json.load(file)
+    # with open('products/fixtures/data/data.json') as file :
+    # data = json.load(file)
     data = Product.objects.all()
     return render(request, 'products\index.html', {'object_list': data})
+
+
 def product_ditail_view(request, pk):
-    #with open('products/fixtures/data/data.json') as file :
-        #data = json.load(file)
+    # with open('products/fixtures/data/data.json') as file :
+    # data = json.load(file)
     data = Product.objects.get(pk=pk)
-    return render(request, 'products\detail.html', {'object':data})
+    return render(request, 'products\detail.html', {'object': data})
+
+
 def category_ditail_view(request, pk):
     data = Category.objects.get(pk=pk)
-    return render(request, 'products\detail.html', {'object':data})
-
+    return render(request, 'products\detail.html', {'object': data})
